09933004b0322a26c5d29b3d4a9abd4d66f076eb
Thomas Eikhaugen <Thomas.Eikhaugen@gmail.com>
2025-03-06 04:22:15 +0100

    feat: added min height to main

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -4,9 +4,9 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main >
+      <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
